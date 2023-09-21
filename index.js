ccd70f4fd252611461a054a3c97fffdb43a036e8
Ian <ian.were@student.moringaschool.com>
2023-09-21 08:37:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  const hqLocation = 42;
+  return Math.abs(pickupLocation - hqLocation);
+}
+function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    const feetperBlock = 264;
+    return blocks * feetperBlock;
+}
+function distanceTravelledInFeet(startBlock, endBlock) {
+  const blocksTraveled = Math.abs(endBlock - startBlock)
+  const feetPerBlock = 264;
+  return blocksTraveled * feetPerBlock;
+}
+function calculatesFarePrice(startBlock, destinationBlock) {
+  const distanceInFeet = distanceTravelledInFeet(startBlock, destinationBlock);
+
+  if(distanceInFeet <= 400) {
+    return 0; 
+} else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+  return (distanceInFeet - 400) * 0.02;
+} else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+  return 25;  
+}else {
+ return 'cannot travel that far';
+}
+
+}
